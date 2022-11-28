b91ba8c4adc00ea4f53dbe99475be1ff12a8fd3a
henquesz <henquesvinicius@gmail.com>
2022-11-27 23:44:09 -0300

    first commit

---
+++ angular-weather-widget/src/app/Components/weather-widget-main/weather-widget-main.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-widget-main',
+  templateUrl: './weather-widget-main.component.html',
+  styleUrls: ['./weather-widget-main.component.css']
+})
+export class WeatherWidgetMainComponent implements OnInit {
+
+  WeatherData:any;
+  constructor() { }
+
+  ngOnInit() {
+    this.WeatherData = {
+      main : {},
+      isDay: true
+    };
+    this.getWeatherData();
+    console.log(this.WeatherData);
+  }
+
+  getWeatherData(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat=-23.5194726&lon=-46.5985972&appid=07bbce9208ba2e983b1c145eb654af71')
+    .then(response=>response.json())
+    .then(data=>{this.setWeatherData(data);})
+
+    // let data = JSON.parse('{"coord":{"lon":72.85,"lat":19.01},"weather":[{"id":721,"main":"Haze","description":"haze","icon":"50n"}],"base":"stations","main":{"temp":297.15,"feels_like":297.4,"temp_min":297.15,"temp_max":297.15,"pressure":1013,"humidity":69},"visibility":3500,"wind":{"speed":3.6,"deg":300},"clouds":{"all":20},"dt":1580141589,"sys":{"type":1,"id":9052,"country":"IN","sunrise":1580089441,"sunset":1580129884},"timezone":19800,"id":1275339,"name":"Mumbai","cod":200}');
+    // this.setWeatherData(data);
+  }
+
+  setWeatherData(data){
+    this.WeatherData = data;
+    let sunsetTime = new Date(this.WeatherData.sys.sunset * 1000);
+    this.WeatherData.sunset_time = sunsetTime.toLocaleTimeString();
+    let currentDate = new Date();
+    this.WeatherData.isDay = (currentDate.getTime() < sunsetTime.getTime());
+    this.WeatherData.temp_celcius = (this.WeatherData.main.temp - 273.15).toFixed(0);
+    this.WeatherData.temp_min = (this.WeatherData.main.temp_min - 273.15).toFixed(0);
+    this.WeatherData.temp_max = (this.WeatherData.main.temp_max - 273.15).toFixed(0);
+    this.WeatherData.pressure = (this.WeatherData.main.pressure).toFixed(0);
+    this.WeatherData.temp_feels_like = (this.WeatherData.main.feels_like - 273.15).toFixed(0);
+    this.WeatherData.winds = (this.WeatherData.wind.speed).toFixed(0);
+    this.WeatherData.windd = (this.WeatherData.wind.deg).toFixed(0);
+    this.WeatherData.humidity = (this.WeatherData.main.humidity).toFixed(0);
+  }
+
+}
